a3ca5870d81c04d701acfdec0e565287a6aeebc9
vladus80 <utru@inbox.ru>
2020-10-25 22:13:34 +0300

    Реализован метод formatName Печатает массив абонентов телефонной книги

--- src/PhoneBook.java
+++ src/PhoneBook.java
@@ -4,9 +4,12 @@ public class PhoneBook {
         //Добавить считывание ввода пользователя в цикле
 
 
+        System.out.println(formatName("петров                  игорь            валерьевич   "));
+
         String[][] users =
                 {
-                        {"Иванов Иван Иванович", "79102553687"},{"ddfddf","154135541"}
+                        {"Иванов Иван Иванович", "79102553687"},
+                        {"ddfddf","154135541"}
                 };
 
         list(users);
@@ -16,12 +19,64 @@ public class PhoneBook {
         return true;
     }
 
+    public static boolean checkNameInBook(String name, String[][] book){
+
+        boolean nameInBook = true;
+
+        for (int i = 0; i < book.length ; i++){
+            System.out.print(book[i][0] +": "+ book[i][1] +"\n");
+
+            if(name.equals(book[i][0])){
+
+                nameInBook = true;
+            }else {
+
+                nameInBook = false;
+            }
+
+
+        }
+
+
+        return nameInBook;
+
+    }
+
     public static boolean checkName(String name) {
-        return true;
+
+        boolean nameNormal;
+
+        String[] nameArr = name.split(" ");
+        if(nameArr.length > 2){
+
+            System.out.println();
+            nameNormal = false;
+        }else {
+            nameNormal = true;
+        }
+
+        return nameNormal;
     }
 
     public static String formatName(String name) {
-        return "";
+
+        String[] nameArr = name.split("\\s+");
+        String formatName = "";
+
+        if(nameArr.length == 3){
+
+            String nameFamUpCase = String.valueOf( nameArr[0].charAt(0)).toUpperCase()+nameArr[0].substring(1, nameArr[0].length());
+            String nameNameUpCase = String.valueOf( nameArr[1].charAt(0)).toUpperCase()+nameArr[1].substring(1, nameArr[1].length());
+            String nameOtUpCase = String.valueOf( nameArr[2].charAt(0)).toUpperCase()+nameArr[2].substring(1, nameArr[2].length());
+
+            formatName = nameFamUpCase.trim() + " " + nameNameUpCase.trim() + " " + nameOtUpCase.trim();
+
+        }else{
+
+            System.out.print("Неверный формат имени. Введите в формате: Фамилия Имя Отчество");
+        }
+
+        return formatName;
     }
 
     public static String formatPhoneNumber(String number) {
@@ -47,13 +102,12 @@ public class PhoneBook {
         //print phone book
 
         for (int i = 0; i < book.length ; i++){
-           // System.out.print(book[i][0] +"\n" );
+            System.out.print(book[i][0] +": "+ book[i][1] +"\n");
             for(int j=0; j < book[i].length; j++){
 
-                System.out.print(book[i][0] +": "+ book[i][1] +"\n" );
+                //System.out.print(book[i][0] +": "+ book[i][1] +"\n" );
             }
-            //System.out.print(book[0][i] +": "+   book[1][i] + "\n" );
-            //System.out.println();
+
         }
 
     }
